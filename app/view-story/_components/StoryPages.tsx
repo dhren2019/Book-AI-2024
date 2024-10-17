abec31d73d9aeb0cde76c9fb1ec1c6f13bcbe402
Dhren <roglobar10@gmail.com>
2024-10-17 09:28:15 +0200

    Bug fixed

--- app/view-story/_components/StoryPages.tsx
+++ app/view-story/_components/StoryPages.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { MdPlayCircleFilled } from "react-icons/md";
 
-function StoryPages({storyChapter}:any) {
+function StoryPages({ storyChapter }: any) {
 
-  const playSpeech=(text:string)=>{
-    const synth=window?.speechSynthesis;
-    const textToSpeech=new SpeechSynthesisUtterance(text);
+  const playSpeech = (text: string) => {
+    const synth = window?.speechSynthesis;
+    const textToSpeech = new SpeechSynthesisUtterance(text);
     synth.speak(textToSpeech);
   }
+
   return (
     <div>
-        <h2 className='text-2xl fontbold text-primary flex justify-between'>{storyChapter?.chapter_title}
-        <span className='text-3xl cursor-pointer' onClick={()=>playSpeech(storyChapter?.chapter_text)}><MdPlayCircleFilled /></span>
-        </h2>
-        <p className='text-lg p-10 mt-3 rounded-lg bg-slate-100 line-clamp-[10]'>{storyChapter?.chapter_text}</p>
+      <h2 className='text-2xl font-bold text-primary flex justify-between'>
+        {storyChapter?.chapter_title}
+        <span className='text-3xl cursor-pointer' onClick={() => playSpeech(storyChapter?.chapter_text)}>
+          <MdPlayCircleFilled />
+        </span>
+      </h2>
+      <p className='text-gray-900 text-lg p-10 mt-3 rounded-lg bg-slate-100'>
+        {/* Removido line-clamp-[10] y cambiado color del texto */}
+        {storyChapter?.chapter_text}
+      </p>
     </div>
   )
 }
