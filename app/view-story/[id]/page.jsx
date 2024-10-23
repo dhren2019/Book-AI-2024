1f00d3c1021008a308bc20a86bcff7199f0ee7d2
Dhren <roglobar10@gmail.com>
2024-10-23 12:44:48 +0200

    Added new features

--- app/view-story/[id]/page.jsx
+++ app/view-story/[id]/page.jsx
@@ -8,33 +8,35 @@ import BookCoverPage from '../_components/BookCoverPage'
 import StoryPages from '../_components/StoryPages'
 import LastPage from '../_components/LastPage'
 import { Button } from '@nextui-org/button'
-import { IoIosArrowDroprightCircle, IoIosArrowDropleftCircle } from "react-icons/io";
-function ViewStory({ params }) {
+import { IoIosArrowDroprightCircle, IoIosArrowDropleftCircle, IoIosDownload, IoIosExpand, IoIosDocument } from "react-icons/io";
 
+function ViewStory({ params }) {
   const [story, setStory] = useState();
   const bookRef = useRef();
   const [count, setCount] = useState(0);
+
   useEffect(() => {
-    console.log(params.id)
     getStory();
   }, [])
 
   const getStory = async () => {
     const result = await db.select().from(StoryData)
       .where(eq(StoryData.storyId, params.id));
-
-    console.log(result[0]);
     setStory(result[0]);
   }
 
   return (
-    <div className='p-10 md:px-20 lg:px-40 flex justify-center  flex-col '>
-      <h2 className='font-bold text-4xl text-center p-10 bg-primary text-white'>{story?.output?.story_cover?.title}</h2>
-      <div className='relative '>
-        {/* @ts-ignore */}
-        <HTMLFlipBook width={500} height={500}
+    <div className='p-4 md:px-10 lg:px-20 flex justify-center items-center flex-col min-h-screen'>
+      <h2 className='font-bold text-3xl md:text-4xl text-center p-6 bg-primary text-white rounded-md w-full max-w-3xl'>{story?.output?.story_cover?.title}</h2>
+      <div className='relative flex justify-center items-center mt-10 w-full'>
+        {/* Ajuste del tamaño del libro para adaptarse a diferentes dispositivos */}
+        <HTMLFlipBook 
+          width={window.innerWidth < 768 ? 300 : 500} 
+          height={window.innerWidth < 768 ? 400 : 500}
+          maxWidth={600}
+          maxHeight={700}
           showCover={true}
-          className='mt-10'
+          className='mt-10 shadow-lg'
           useMouseEvents={false}
           ref={bookRef}
         >
@@ -43,13 +45,13 @@ function ViewStory({ params }) {
           </div>
           {
             [...Array(story?.output?.chapters?.length)].map((item, index) => (
-              <div key={index} className='bg-white p-10 border'>
+              <div key={index} className='bg-white p-10 border rounded-md'>
                 <StoryPages storyChapter={story?.output.chapters[index]} />
               </div>
             ))
           }
         </HTMLFlipBook>
-        {count != 0 && <div className='absolute -left-5 top-[250px]'
+        {count != 0 && <div className='absolute left-0 md:-left-5 top-1/2 transform -translate-y-1/2'
           onClick={() => {
             bookRef.current.pageFlip().flipPrev();
             setCount(count - 1)
@@ -58,12 +60,27 @@ function ViewStory({ params }) {
           <IoIosArrowDropleftCircle className='text-[40px] text-primary cursor-pointer' />
         </div>}
 
-        {count != (story?.output.chapters?.length - 1) && <div className='absolute right-0 top-[250px]' onClick={() => {
+        {count != (story?.output.chapters?.length - 1) && <div className='absolute right-0 md:-right-5 top-1/2 transform -translate-y-1/2' onClick={() => {
           bookRef.current.pageFlip().flipNext();
           setCount(count + 1)
         }}>
           <IoIosArrowDroprightCircle className='text-[40px] text-primary cursor-pointer' />
         </div>}
+      </div>
+      {/* Iconos adicionales debajo del libro */}
+      <div className='flex justify-center items-center gap-8 mt-8'>
+        <div className='flex flex-col items-center cursor-pointer'>
+          <IoIosDownload className='text-[40px] text-primary' />
+          <span className='mt-2 text-primary'>Descargar</span>
+        </div>
+        <div className='flex flex-col items-center cursor-pointer'>
+          <IoIosExpand className='text-[40px] text-primary' />
+          <span className='mt-2 text-primary'>Ampliar</span>
+        </div>
+        <div className='flex flex-col items-center cursor-pointer'>
+          <IoIosDocument className='text-[40px] text-primary' />
+          <span className='mt-2 text-primary'>Lerctura seguida</span>
+        </div>
       </div>
     </div>
   )
