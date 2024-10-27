fd63bb60ba3ee1a17fef1752369c5ecf0dda51d7
Dhren <51840371+dhren2019@users.noreply.github.com>
2024-10-27 22:00:30 +0100

    Responsive

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,26 +1,45 @@
-import { Button } from '@nextui-org/button'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { Button } from '@nextui-org/button';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 
 function Hero() {
   return (
-    <div className='px-10 md:px-28 lg:px-44 mt-10 h-screen'>
-        <div className='grid grid-cols-1 md:grid-cols-2'>
-            <div>
-                <h2 className='text-[70px] text-primary font-extrabold py-10'>Craft Magical Stories for kids in Minutes</h2>
-                <p className='text-2xl text-primary font-light'>Create fun and personalised stories that bring your child's adventures to life and spark their passion for reading. It only takes a few seconds!</p>
-                <Link href={'/create-story'}>
-                <Button size='lg' color='primary' 
-                className='mt-5 font-bold text-2xl p-8'>Create Story</Button>
-                </Link>
-            </div>
-            <div>
-                <Image src={'/hero.png'} alt='hero' width={700} height={400}/>
-            </div>
+    <div className="container mx-auto px-5 sm:px-10 lg:px-20 xl:px-32 mt-10 min-h-screen flex items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center text-center md:text-left">
+        
+        {/* Texto Principal */}
+        <div className="flex flex-col gap-5 md:gap-8">
+          <h2 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-primary font-extrabold">
+            Craft Magical Stories for Kids in Minutes
+          </h2>
+          <p className="text-base sm:text-lg md:text-xl lg:text-2xl text-primary font-light">
+            Create fun and personalised stories that bring your child's adventures to life and spark their passion for reading. It only takes a few seconds!
+          </p>
+          <Link href={'/create-story'}>
+            <Button
+              size="lg"
+              color="primary"
+              className="mt-5 md:mt-6 lg:mt-8 font-bold text-sm sm:text-base md:text-lg lg:text-xl py-2 sm:py-3 px-4 sm:px-6"
+            >
+              Create Story
+            </Button>
+          </Link>
         </div>
+
+        {/* Imagen del Hero */}
+        <div className="flex justify-center md:justify-end">
+          <Image
+            src="/hero.png"
+            alt="hero"
+            width={500}
+            height={300}
+            className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- app/create-story/page.tsx
+++ app/create-story/page.tsx
@@ -1,174 +1,173 @@
-"use client"
-import React, { useContext, useState } from 'react'
-import StorySubjectInput from './_components/StorySubjectInput'
-import StoryType from './_components/StoryType'
-import AgeGroup from './_components/AgeGroup'
-import ImageStyle from './_components/ImageStyle'
-import { Button } from '@nextui-org/button'
-import { chatSession } from '@/config/GeminiAi'
-import { db } from '@/config/db'
-import { StoryData, Users } from '@/config/schema'
+"use client";
+import React, { useContext, useState } from 'react';
+import StorySubjectInput from './_components/StorySubjectInput';
+import StoryType from './_components/StoryType';
+import AgeGroup from './_components/AgeGroup';
+import ImageStyle from './_components/ImageStyle';
+import { Button } from '@nextui-org/button';
+import { chatSession } from '@/config/GeminiAi';
+import { db } from '@/config/db';
+import { StoryData, Users } from '@/config/schema';
 //@ts-ignore
 import uuid4 from "uuid4";
-import CustomLoader from './_components/CustomLoader'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
-import { toast } from 'react-toastify'
-import { useUser } from '@clerk/nextjs'
-import { UserDetailContext } from '../_context/UserDetailConext'
-import { eq } from 'drizzle-orm'
+import CustomLoader from './_components/CustomLoader';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import { useUser } from '@clerk/nextjs';
+import { UserDetailContext } from '../_context/UserDetailConext';
+import { eq } from 'drizzle-orm';
 
-const CREATE_STORY_PROMPT=process.env.NEXT_PUBLIC_CREATE_STORY_PROMPT
-export interface fieldData{
-  fieldName:string,
-  fieldValue:string
+const CREATE_STORY_PROMPT = process.env.NEXT_PUBLIC_CREATE_STORY_PROMPT;
+
+export interface fieldData {
+  fieldName: string;
+  fieldValue: string;
 }
-export interface formDataType{
-  storySubject:string,
-  storyType:string,
-  imageStyle:string,
-  ageGroup:string,
+
+export interface formDataType {
+  storySubject: string;
+  storyType: string;
+  imageStyle: string;
+  ageGroup: string;
   emotion: string; // Emotions
 }
+
 function CreateStory() {
+  const [formData, setFormData] = useState<formDataType>();
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const notify = (msg: string) => toast(msg);
+  const notifyError = (msg: string) => toast.error(msg);
+  const { user } = useUser();
+  const { userDetail, setUserDetail } = useContext(UserDetailContext);
 
-  const [formData,setFormData]=useState<formDataType>();
-  const [loading,setLoading]=useState(false);
-  const router=useRouter();
-  const notify = (msg:string) => toast(msg);
-  const notifyError = (msg:string) => toast.error(msg);
-  const {user}=useUser();
-  const {userDetail,setUserDetail}=useContext(UserDetailContext);
-
-  /**
-   * used to add data to form
-   * @param data 
-   */
-  const onHandleUserSelection=(data:fieldData)=>{
-    setFormData((prev:any)=>({
+  // Handle user input selection and add to form data
+  const onHandleUserSelection = (data: fieldData) => {
+    setFormData((prev: any) => ({
       ...prev,
-      [data.fieldName]:data.fieldValue
+      [data.fieldName]: data.fieldValue
     }));
-    console.log(formData)
-  }
+    console.log(formData);
+  };
 
-  const GenerateStory=async()=>{
+  // Generate Story Function
+  const GenerateStory = async () => {
+    if (userDetail.credit <= 0) {
+      notifyError('You dont have enough credits!');
+      return;
+    }
 
-    if(userDetail.credit<=0)
-      {
-        notifyError('You dont have enough credits!');
-        return ;
-      }
+    setLoading(true);
+    const FINAL_PROMPT = CREATE_STORY_PROMPT
+      ?.replace('{ageGroup}', formData?.ageGroup ?? '')
+      .replace('{storyType}', formData?.storyType ?? '')
+      .replace('{emotion}', formData?.emotion ?? '')
+      .replace('{storySubject}', formData?.storySubject ?? '')
+      .replace('{imageStyle}', formData?.imageStyle ?? '');
 
-    setLoading(true)
-    const FINAL_PROMPT=CREATE_STORY_PROMPT
-    ?.replace('{ageGroup}',formData?.ageGroup??'')
-    .replace('{storyType}',formData?.storyType??'')
-    .replace('{emotion}', formData?.emotion ?? '')
-    .replace('{storySubject}',formData?.storySubject??'')
-    .replace('{imageStyle}',formData?.imageStyle??'')
-    //Generate AI Story
-      try{
-        const result=await chatSession.sendMessage(FINAL_PROMPT);
-        const story=JSON.parse(result?.response.text().replace(/(})(,?)(\n *\})/g, "$1,"));
-       
-        // const jsonString=result?.response.text().replace(/":\s*"(.*?)"/g, '": "$1",')   // Adds comma after string values
-        // .replace(/":\s*(\d+)(?=\s*)/g, '": $1,') // Adds comma after numeric values
-        // .replace(/,\s*}/g, ' }'); // Removes the last comma before closing brace
-       // const story=JSON.parse(jsonString)
-       
-         //Generate Image
+    try {
+      // Generate AI Story
+      const result = await chatSession.sendMessage(FINAL_PROMPT);
+      const story = JSON.parse(result?.response.text().replace(/(})(,?)(\n *\})/g, "$1,"));
 
-        const imageResp=await axios.post('/api/generate-image',{
-          prompt:'Add text with  title:'+story?.story_cover?.title+
-          " in bold text for book cover, "+story?.story_cover?.image_prompt
-        })
-        const AiImageUrl=imageResp?.data?.imageUrl
-        
-        const imageResult=await axios.post('/api/save-image',{
-          url:AiImageUrl
-        });
+      // Generate Image
+      const imageResp = await axios.post('/api/generate-image', {
+        prompt: 'Add text with title: ' + story?.story_cover?.title +
+          " in bold text for book cover, " + story?.story_cover?.image_prompt
+      });
 
-        const FirebaseStorageImageUrl=imageResult.data.imageUrl;
-      const resp:any= await SaveInDB(result?.response.text(),FirebaseStorageImageUrl);
-        notify("Story generated")
-       await UpdateUserCredits();
-        router?.replace('/view-story/'+resp[0].storyId)
-        setLoading(false);
-      }catch(e){
-        console.log(e)
-        notifyError('Server Error, Try again')
-        setLoading(false);
-      }
-    
-  }
+      const AiImageUrl = imageResp?.data?.imageUrl;
 
-  /**
-   * Save Data in Database
-   * @param output AI Output
-   * @returns 
-   */
-  const SaveInDB=async(output:string,imageUrl:string)=>{
-    const recordId=uuid4();
-    setLoading(true)
-    try{
-      const result=await db.insert(StoryData).values({
-          storyId:recordId,
-          ageGroup:formData?.ageGroup,
-          imageStyle:formData?.imageStyle,
-          storySubject:formData?.storySubject,
-          storyType:formData?.storyType,
-          output:JSON.parse(output),
-          coverImage:imageUrl,
-          userEmail:user?.primaryEmailAddress?.emailAddress,
-          userImage:user?.imageUrl,
-          userName:user?.fullName
-      }).returning({storyId:StoryData?.storyId})
+      const imageResult = await axios.post('/api/save-image', {
+        url: AiImageUrl
+      });
+
+      const FirebaseStorageImageUrl = imageResult.data.imageUrl;
+      const resp: any = await SaveInDB(result?.response.text(), FirebaseStorageImageUrl);
+      notify("Story generated");
+      await UpdateUserCredits();
+      router?.replace('/view-story/' + resp[0].storyId);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+      notifyError('Server Error, Try again');
+      setLoading(false);
+    }
+  };
+
+  // Save Story Data in Database
+  const SaveInDB = async (output: string, imageUrl: string) => {
+    const recordId = uuid4();
+    setLoading(true);
+    try {
+      const result = await db.insert(StoryData).values({
+        storyId: recordId,
+        ageGroup: formData?.ageGroup,
+        imageStyle: formData?.imageStyle,
+        storySubject: formData?.storySubject,
+        storyType: formData?.storyType,
+        output: JSON.parse(output),
+        coverImage: imageUrl,
+        userEmail: user?.primaryEmailAddress?.emailAddress,
+        userImage: user?.imageUrl,
+        userName: user?.fullName
+      }).returning({ storyId: StoryData?.storyId });
       setLoading(false);
       return result;
+    } catch (e) {
+      setLoading(false);
     }
-    catch(e)
-    {
-        setLoading(false);
-    } 
-  }
+  };
 
-
-  const UpdateUserCredits=async()=>{
-    const result=await db.update(Users).set({
-      credit:Number(userDetail?.credit-1)
-    }).where(eq(Users.userEmail,user?.primaryEmailAddress?.emailAddress??''))
-    .returning({id:Users.id})
-  }
+  // Update User Credits in Database
+  const UpdateUserCredits = async () => {
+    const result = await db.update(Users).set({
+      credit: Number(userDetail?.credit - 1)
+    }).where(eq(Users.userEmail, user?.primaryEmailAddress?.emailAddress ?? ''))
+      .returning({ id: Users.id });
+  };
 
   return (
-    <div className='p-10 md:px-20 lg:px-40'>
-      <h2 className='font-extrabold text-[70px] text-primary text-center'>CREATE YOUR STORY</h2>
-      <p className='text-2xl text-primary text-center'>Unlock your creativity with AI: Craft stories like never before!Let our AI bring your imagination to life, one story at a time.</p>
+    <div className='w-full min-h-screen p-6 sm:p-10 bg-gradient-to-b from-gray-800 to-black text-white overflow-hidden'>
+      <h2 className='font-extrabold text-3xl sm:text-4xl md:text-5xl lg:text-[60px] text-primary text-center mb-8'>
+        CREATE YOUR STORY
+      </h2>
+      <p className='text-base sm:text-lg md:text-xl lg:text-2xl text-primary text-center mb-10 px-4 md:px-0'>
+        Unlock your creativity with AI: Craft stories like never before! Let our AI bring your imagination to life, one story at a time.
+      </p>
 
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-10 mt-14'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 gap-8 md:gap-12 mt-10'>
         {/* Story Subject  */}
-          <StorySubjectInput userSelection={onHandleUserSelection}/>
+        <div className='flex flex-col items-center p-4 md:p-8 bg-gray-900 rounded-xl shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out w-full h-auto'>
+          <StorySubjectInput userSelection={onHandleUserSelection} />
+        </div>
         {/* Story Type  */}
-          <StoryType userSelection={onHandleUserSelection}/>
+        <div className='flex flex-col items-center p-4 md:p-8 bg-gray-900 rounded-xl shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out w-full h-auto'>
+          <StoryType userSelection={onHandleUserSelection} customClass='grid grid-cols-2 gap-4 text-xs sm:text-sm md:text-base' />
+        </div>
         {/* Age Group  */}
-          <AgeGroup userSelection={onHandleUserSelection}/>
+        <div className='flex flex-col items-center p-4 md:p-8 bg-gray-900 rounded-xl shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out w-full h-auto'>
+          <AgeGroup userSelection={onHandleUserSelection} customClass='grid grid-cols-2 gap-4 text-xs sm:text-sm md:text-base' />
+        </div>
         {/* Image Style  */}
-         <ImageStyle userSelection={onHandleUserSelection}/>
+        <div className='flex flex-col items-center p-4 md:p-8 bg-gray-900 rounded-xl shadow-xl hover:shadow-2xl transition-shadow duration-300 ease-in-out w-full h-auto'>
+          <ImageStyle userSelection={onHandleUserSelection} customClass='grid grid-cols-2 gap-4 text-xs sm:text-sm md:text-base' />
+        </div>
       </div>
 
-      <div className='flex justify-end my-10 flex-col items-end'>
-        <Button color='primary' 
-        disabled={loading}
-        className='p-10 text-2xl'
-        onClick={GenerateStory}>
-          Generate Story</Button>
-          <span>1 Credit will user</span>
+      <div className='flex flex-col sm:flex-row justify-center items-center gap-6 mt-16 w-full'>
+        <Button color='primary'
+          disabled={loading}
+          className='w-full sm:w-auto px-8 py-6 sm:px-12 sm:py-8 text-base sm:text-lg lg:text-xl font-bold bg-blue-500 hover:bg-blue-600 transition-colors duration-300 ease-in-out rounded-lg shadow-md hover:shadow-lg'>
+          Generate Story
+        </Button>
+        <span className='text-sm text-primary sm:ml-4'>1 Credit will be used</span>
       </div>
-      <CustomLoader isLoading={loading}/>
 
+      <CustomLoader isLoading={loading} />
     </div>
-  )
+  );
 }
 
-export default CreateStory
+export default CreateStory;
